317bdf83703ecf6c075048b5735cce6bcd78ab67
shadow <shadowknight09812@gmail.com>
2026-09-09 16:56:23 +0530

    Fix IllegalStateChangeError by removing redundant session.close inside async with context manager

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -43,7 +43,4 @@ Base = declarative_base()
 
 async def get_db():
     async with AsyncSessionLocal() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+        yield session
